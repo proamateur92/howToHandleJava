0d38076e359c029063bff081c743c27fac766a80
DongbokLee <kkasu1992@gmail.com>
2021-04-21 01:26:24 +0900

    chapter7 서블릿 비즈니스 로직 223P~236P

---
+++ pro07/src/sec01/ex01/MemberServlet.java
@@ -0,0 +1,43 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member") 
+public class MemberServlet extends HttpServlet{
+
+	public void init() {
+		System.out.println("init 메서드 실행");
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+	throws ServletException, IOException{
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		MemberDAO dao = new MemberDAO();
+		List<MemberVO> list = dao.memberList();
+		
+		out.print("<html><body>");
+		out.print("<table border=1><tr align='center' bgcolor='lightgreen'>");
+		out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td></tr>");
+		
+		for(int i=0; i<list.size(); i++) {
+			MemberVO memberVO = list.get(i);
+			String id = memberVO.getId();
+			String pw = memberVO.getPwd();
+			String name = memberVO.getName();
+			String email = memberVO.getEmail();
+			Date joinDate = memberVO.getJoinDate();
+			out.print("<tr><td>"+ id + "</td><td>" + pw + "</td><td>" + name + "</td><td> "+ email + "</td><td>" + joinDate + "</td></tr>");
+		}
+		out.print("</table></body></html>");
+	}
+}
