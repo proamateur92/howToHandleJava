5fa62e42180a91812f14b1c528928451fa6e2ed9
DongbokLee <kkasu1992@gmail.com>
2021-05-07 01:09:45 +0900

    9.6 세션을 이용한 로그인 예제

--- pro09/src/sec04/ex01/SessionTest5.java
+++ pro09/src/sec04/ex01/SessionTest5.java
@@ -10,15 +10,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/login8")
-public class SessionTest04 extends HttpServlet {
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+@WebServlet("/login")
+public class SessionTest5 extends HttpServlet{
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+	throws ServletException, IOException{
 		doHandle(request, response);
 	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+	throws ServletException, IOException{
 		doHandle(request, response);
 	}
 	
@@ -33,21 +33,20 @@ public class SessionTest04 extends HttpServlet {
 		if(session.isNew()) {
 			if(user_id!=null) {
 				session.setAttribute("user_id", user_id);
-				out.println("<a href='login8'>로그인 상태 확인</a>");
+				String url = response.encodeURL("login");
+				out.print("<a href="+url+">로그인 상태 확인</a>");
 			} else {
-				out.print("<a href='login2.html'>다시 로그인 하세요!!!!!!!</a>");
+				out.print("<a href='login2.html'> 다시 로그인 하세요!!!! </a>");
 				session.invalidate();
 			}
-		}
-		else {
+		} else {
 			user_id = (String)session.getAttribute("user_id");
 			if(user_id!=null&&user_id.length()!=0) {
-				out.print("안녕하세요 " + user_id + "님!!!");
+				out.print(user_id+"님 안녕하세요!!!!!");
 			} else {
-				out.print("<a href='login2.html'>다시 로그인 하세요옷!!!!!</a>");
+				out.print("<a href='login2.html'>다시 로그인 하세요!!!!!</a>");
 				session.invalidate();
 			}
-		}
-		
+	}
 	}
 }
---
+++ pro09/src/sec05/ex01/MemberDAO.java
@@ -0,0 +1,150 @@
+package sec05.ex01;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class MemberDAO {
+	private PreparedStatement pstmt;
+	private Connection con;
+	private DataSource dataFactory;
+	
+	public MemberDAO() {
+		try {
+			Context ctx = new InitialContext();
+			Context envConetext = (Context)ctx.lookup("java:/comp/env");
+			dataFactory = (DataSource)envConetext.lookup("jdbc/oracle");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List<MemberVO> listMembers() {
+		List<MemberVO> list = new ArrayList<MemberVO>();
+
+		try {
+//			connDB();
+			con = dataFactory.getConnection();
+			
+			String query = "select * from t_member order by joinDate desc";
+			System.out.println("prepareStatement: " + query);
+			
+			pstmt = con.prepareStatement(query);
+			ResultSet rs = pstmt.executeQuery();
+			
+			
+			while(rs.next()) {
+				
+				String id = rs.getString("id");
+				String pwd = rs.getString("pwd");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				Date joinDate = rs.getDate("joinDate");
+				
+				MemberVO vo = new MemberVO();
+				
+				vo.setId(id);
+				vo.setPwd(pwd);
+				vo.setEmail(email);
+				vo.setName(name);
+				vo.setJoinDate(joinDate);
+				
+				list.add(vo);
+			}
+			rs.close();
+			pstmt.close();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public void delMember(String id) {
+		try {
+			con = dataFactory.getConnection();
+			String query = "delete from t_member where id=?";
+			System.out.println("prepareStatemet: " + query);
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, id);
+			pstmt.executeQuery();
+			pstmt.close();
+		}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+	}
+
+	public void addMember(MemberVO memberVO) {
+		try {
+			con = dataFactory.getConnection();
+			String id = memberVO.getId();
+			String pwd = memberVO.getPwd();
+			String name = memberVO.getName();
+			String email = memberVO.getEmail();
+			Date joinDate = memberVO.getJoinDate();
+		
+			String query = "insert into t_member";
+			query += " (id,pwd,name,email)";
+			query += "values(?,?,?,?)";
+			System.out.println("PrepareStatement: " + query);
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, id);
+			pstmt.setString(2, pwd);
+			pstmt.setString(3, name);
+			pstmt.setString(4, email);
+			pstmt.executeUpdate();
+			pstmt.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/*
+	private void connDB() {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "scott";
+		String pwd = "tiger";
+		
+		try {
+			Class.forName(driver);
+			System.out.println("Oracle driver 로딩 성공");
+			con = DriverManager.getConnection(url, user, pwd);
+			System.out.println("Connection 생성 성공");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	*/
+	
+	public boolean isExisted(MemberVO memberVO) {
+		boolean result = false;
+		String id = memberVO.getId();
+		String pwd = memberVO.getPwd();
+		try {
+			con = dataFactory.getConnection();
+			
+			String query = "select decode(count(*),1,'true','false') as result from t_member";
+			query += " where id=? and pwd=?";
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, id);
+			pstmt.setString(2, pwd);
+			ResultSet rs = pstmt.executeQuery();
+			rs.next();
+			result = Boolean.parseBoolean(rs.getString("result"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+}
+
