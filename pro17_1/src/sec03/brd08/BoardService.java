ed0811860a72c3f25d69a28be2b2c9ff02c0823d
DongbokLee <kkasu1992@gmail.com>
2021-07-19 22:59:24 +0900

    17 게시판 기본 기능 구현(글 작성, 상세, 수정, 삭제, 페이징) 2021/07/19

---
+++ pro17_1/src/sec03/brd08/BoardService.java
@@ -0,0 +1,56 @@
+package sec03.brd08;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BoardService {
+	BoardDAO boardDAO;
+	
+	public BoardService() {
+		boardDAO = new BoardDAO();
+	}
+	
+//	public List<ArticleVO> listArticles() {
+//		List<ArticleVO> articlesList = boardDAO.selectAllArticles();
+//		return articlesList;
+//	}
+	
+	public int addArticle(ArticleVO article) {
+		return boardDAO.insertNewArticle(article);
+	}
+
+	public ArticleVO viewArticle(int articleNO) {
+		ArticleVO article = null;
+		article = boardDAO.selectArticle(articleNO);
+		return article;
+	}
+
+	public void modArticle(ArticleVO article) {
+		boardDAO.updateArticle(article);
+	}
+
+	public List<Integer> removeArticle(int articleNO) {
+		List<Integer> articleNOList = boardDAO.selectRemovedArticles(articleNO);
+		boardDAO.deleteArticle(articleNO);
+		return articleNOList;
+	}
+
+	public int addReply(ArticleVO article) {
+		return boardDAO.insertNewArticle(article);
+	}
+
+	public Map listArticles(Map pagingMap) {
+		Map articlesMap = new HashMap();
+		List<ArticleVO> articlesList = boardDAO.selectAllArticles(pagingMap);
+		System.out.println("service==============");
+		System.out.println("0: "+articlesList.get(0));
+		System.out.println("1: "+articlesList.get(1));
+		int totArticles = boardDAO.selectToArticles();
+		System.out.println("tot: " + totArticles);
+		articlesMap.put("articlesList", articlesList);
+		articlesMap.put("totArticles",totArticles);
+		return articlesMap;
+	}
+	
+}
